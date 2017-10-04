5bfec049c825424a4aff4d3289d039caf3251eab
joe jiao <yingjunjiao@outlook.com>
2017-10-04 16:33:19 +0800

    test http2

--- transport/src/main/java/io/unreach/israel/transport/ChannelHandler.java
+++ transport/src/main/java/io/unreach/israel/transport/ChannelHandler.java
@@ -1,9 +1,28 @@
 package io.unreach.israel.transport;
 
+/**
+ * 远程调用执行接口
+ *
+ * @param <T>
+ * @author joe
+ */
 public interface ChannelHandler<T> {
 
+    /**
+     * 执行方法调用
+     *
+     * @param serviceId
+     * @param methodName
+     * @param params
+     * @return
+     */
     public Object invoke(String serviceId, String methodName, Object[] params);
 
+    /**
+     * 获取客户端的channel 实现
+     *
+     * @return
+     */
     public T getChannel();
 
 }
